cffb71bf7296c10e947d7412da3bac08eb6202e7
Nono_IA <noe.bouboul06@gmail.com>
2025-11-07 16:25:00 +0100

    Implémentation de changement de direction : Droite() Gauche() Arriere()

--- biped.ino
+++ biped.ino
@@ -3,14 +3,45 @@
 Servo servogauchepied, servodroitepied, servogauchebassin, servodroitebassin;
 void setup()
 {
+    Serial.begin(9600);
+    Serial.flush();
     servodroitepied.attach(4);
     servodroitebassin.attach(5);
     servogauchepied.attach(6);
     servogauchebassin.attach(7);
+   
 }
-int a = 0;
 
-void init()
+int a = 0;       //compteur boucle
+int distD; 
+int distG;// distance obstacle 
+int analogPinG = 1;
+int analogPinD = 0;
+
+void loop()
+{
+    initia();
+    distD = analogRead(analogPinD);
+    distG = analogRead(analogPinG);
+    Serial.print("distG = ");
+    Serial.println(distG);
+    //if (a == 0)
+    //{
+    //     initia();
+    //}
+    //a = a + 1;
+    //arriere();
+    //if (a > 10)
+    //{
+    //    marche();
+    //}
+    //while (a > 0)
+    //{
+    //    initia();
+    //}
+}
+
+void initia()
 {
     servogauchepied.write(60);
     servodroitepied.write(100);
@@ -18,8 +49,7 @@ void init()
     servodroitebassin.write(80);
     servogauchepied.write(80);
     servodroitepied.write(80);
-    delay(1000);
-    a = 1;
+    delay(100);
 }
 
 void marche()
@@ -85,29 +115,97 @@ void course()
     servodroitebassin.write(105);
     servogauchebassin.write(105);
     servodroitepied.write(80);
-    delay(300);
+    delay(200);
 
     'etape6';
     servogauchepied.write(80);
     delay(300);
 }
 
-void loop()
+void droite ()
 {
-    'setup';
-    if (a == 0)
-    {
-        init();
-    }
-    a = a + 1;
-    marche();
-    course();
-    if (a == 9)
-    {
-        marche();
-    }
-    while (a < 10)
-    {
-        inti();
-    }
+    'etape1';
+    servogauchepied.write(110);
+    delay(100);
+    servodroitepied.write(105);
+    delay(500);
+
+    'etape2';
+    servodroitebassin.write(45);
+    servogauchepied.write(80);
+    delay(300);
+
+    'etape3';
+    servodroitepied.write(70);
+    delay(110);
+    servogauchepied.write(55);
+    servodroitepied.write(80);
+    delay(300);
+
+    'etape4';
+    servodroitebassin.write(80);
+    servogauchepied.write(80);
+    delay(300);
+
+}
+
+void gauche ()
+{
+    'etape1';
+    servodroitepied.write(40);
+    delay(150);
+    servogauchepied.write(55);
+    delay(300);
+
+    'etape2';
+    servogauchebassin.write(95);
+    servodroitepied.write(80);
+    delay(300);
+
+    'etape3';
+    servogauchepied.write(80);
+    delay(100);
+    servodroitepied.write(100);
+    delay(300);
+
+    'etape4';
+    servogauchebassin.write(80);
+    servodroitepied.write(80);
+    delay(300);
+
+}
+
+void arriere ()
+{
+    'etape1';
+    servogauchepied.write(110);
+    delay(100);
+    servodroitepied.write(105);
+    delay(500);
+
+    'etape2';
+    servodroitebassin.write(105);
+    servogauchebassin.write(105);
+    servogauchepied.write(80);
+    delay(300);
+
+    'etape3';
+    servodroitepied.write(80);
+    delay(300);
+
+    'etape4';
+    servodroitepied.write(40);
+    delay(150);
+    servogauchepied.write(55);
+    delay(300);
+
+    'etape5';
+    servodroitebassin.write(80);
+    servogauchebassin.write(80);
+    servodroitepied.write(80);
+    delay(300);
+
+    'etape6';
+    servogauchepied.write(80);
+    delay(300);
 }
